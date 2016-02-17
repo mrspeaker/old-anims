6e26ef20962abd858bbaf715ae2a2adc4fad5999
Mr Speaker <mrspeaker@gmail.com>
2016-02-16 19:32:07 -0500

    uncopypaste

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const ctx = document.querySelector("#board").getContext("2d");
 const {width, height} = ctx.canvas;
 
+const CTX = width * 0.5;
+const CTY = height * 0.5;
+
 const PLYGN = (N, ANGLE, R, XCTR, YCTR) => {
   ctx.beginPath();
   for (let J = 0; J < N; J++) {
     const X = R * Math.cos(Math.PI * 2 * J / N + ANGLE) + XCTR;
     const Y = R * Math.sin(Math.PI * 2 * J / N + ANGLE) + YCTR;
 
-    ctx[J === 0 ? "moveTo" : "lineTo"](X, Y);
+    ctx[J ? "lineTo" : "moveTo"](X, Y);
   }
   ctx.closePath();
   ctx.stroke();
@@ -22,24 +25,19 @@ let off = 0;
 let time = 0;
 const DRAW = (dt) => {
   off += 0.001 * dt;
-  time += dt;
-
-  const CTX = width * 0.5;
-  const CTY = height * 0.5;
+  time += dt * 0.1;
 
   PLYGN(5, off, 30, CTX, CTY);
   PLYGN(8, -off, 30, CTX, CTY);
 
-  EXPAND(time / 10, width * 0.5, -height * 0.5);
-  EXPAND(time / 10, width * 0.5, height * 0.5);
-  EXPAND(time / 10, width * 0.5, height * 1.5);
+  EXPAND(time, CTX, -CTY);
+  EXPAND(time, CTX, CTY);
+  EXPAND(time, CTX, height * 1.5);
+  EXPAND(time, -CTX, CTY);
+  EXPAND(time, width * 1.5, CTY);
 
-  EXPAND(time / 10, -width * 0.5, height * 0.5);
-  EXPAND(time / 10, width * 1.5, height * 0.5);
-
-  EXPAND((time - 11000) / 10, CTX, CTY)
-
-  if (time > 11 * 1000) {
+  EXPAND(time - 1100, CTX, CTY);
+  if (time > 1100) {
     time = 0;
   }
 };
